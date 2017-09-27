cc3298aa6ca6d796c16257e93047a7efffba0bc9
Pandaromin <earlaromin@gmail.com>
2017-09-27 09:44:19 -0400

    Ok.. so last commit deleted these

---
+++ Map.cpp
@@ -0,0 +1,352 @@
+/*
+ *  * Author: Earl Steven Aromin 40004997
+ */
+
+
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include "Map.h"
+using std::string;
+using std::vector;
+using std::pair;
+
+Map::Map() {
+    conti = {};
+    edges = {};
+}
+
+Map::Map(string n) {
+    name = n;
+    conti = {};
+    edges = {};
+
+}
+//number of countries. Iterates through continents and gets country nums
+
+int Map::getMapSize() {
+    int mapSize = 0;
+
+    for (int continent = 0; continent < conti.size(); continent++) {
+        mapSize += conti.at(continent)->getCntsSize();
+    }
+
+    return mapSize;
+}
+//flags reachable countries from root. Uses a recursive depth-first search
+
+void Map::flagConnections(Country* root) {
+
+    root->setVisited(true);
+
+    for (int i = 0; i < conti.size(); i++) {
+        for (int j = 0; j < conti[i]->cnts.size(); j++) {
+            if (areAdjacent(*root, *conti[i]->cnts[j])
+                    &&(conti[i]->cnts[j]->getVisited() == false)) {
+                flagConnections(conti[i]->cnts[j]);
+            }
+        }
+    }
+}
+
+//flag connection for continent
+
+void Map::flagContinent(Country* root, Continent* cont) {
+    root->setVisited(true);
+    for (int i = 0; i < cont->cnts.size(); i++) {
+        if (areAdjacent(*root, *cont->cnts[i])
+                && cont->cnts[i]->getVisited() == false)
+ {
+            flagContinent(cont->cnts[i], cont);
+        }
+    }
+}
+
+bool Map::valiCont(Continent* cont) {
+    flagContinent(cont->cnts[0], cont);
+
+    for (int i = 0; i < cont->cnts.size(); i++) {
+        if (cont->cnts[i]->getVisited() == false) {
+            cout << cont->cnts[i]->getName() << " in " << cont->getName() << " cannot be reached" << endl; //that's a 0
+            return false;
+        }
+    }
+
+    cout << cont->getName() << " is connected" << endl; //this is a 1
+    resetFlags();
+    return true;
+}
+
+//sets everything to not visited
+
+void Map::resetFlags() {
+    for (int i = 0; i < conti.size(); i++) {
+        for (int j = 0; j < conti[i]->cnts.size(); j++) {
+            conti[i]->cnts[j]->setVisited(false);
+        }
+    }
+}
+
+/**
+ * 
+ * @param m first country
+ * @param n second country
+ * @return if the two are a pair in the edges vector, regardless of first, it returns true
+ */
+bool Map::areAdjacent(Country m, Country n) {
+    for (int i = 0; i < edges.size(); i++) {
+        if ((edges[i].first.getName() == m.getName() && edges[i].second.getName() == n.getName())
+                || (edges[i].first.getName() == n.getName() && edges[i].second.getName() == m.getName())) {
+            return true;
+        }
+    }
+    return false;
+
+}
+//checks if all maps are reachable
+
+bool Map::validate() {
+
+    flagConnections(conti[0]->cnts[0]); //flags reachable ones
+
+    for (int i = 0; i < conti.size(); i++) {
+        for (int j = 0; j < conti[i]->cnts.size(); j++) {
+            if (conti[i]->cnts[j]->getVisited() == false) {
+                cout << conti[i]->cnts[j]->getName() << " cannot be reached" << endl; //that's a 0
+                return false;
+            }
+        }
+    }
+    cout << "Map is connected" << endl; //this is a 1
+    resetFlags();
+    return true;
+}
+//creates a pair of countries that will be used for validation
+
+void Map::link(Country m, Country n) {
+    pair < Country, Country> k;
+    k = std::make_pair(m, n);
+    edges.push_back(k);
+}
+
+void Map::addContinent(Continent* c) {
+    conti.push_back(c);
+}
+
+string Map::getName() {
+    return name;
+}
+
+string Map::toString() {
+    return "Name: " + name + "\n";
+}
+
+pair<Country, Country> Map::getEdge(int index) {
+    return edges[index];
+}
+//just prints all continent 
+
+string Map::printContinents() {
+    string temp;
+    if (conti.empty()) {
+        return (name + " is an empty Map");
+    } else {
+        for (int i = 0; i < conti.size(); i++) {
+            temp += conti[i]->getName() + "\n";
+        }
+        return temp;
+    }
+}
+//returns continent from index
+
+Continent* Map::getContinent(int index) {
+    return conti[index];
+}
+
+//returns continent from  name
+
+Continent* Map::getContinent(string contname) {
+
+    for (int i = 0; i < conti.size(); i++) {
+        if (conti[i]->getName() == (contname))
+
+            return conti[i];
+    }
+}
+//... checks if same country name is in different continents
+
+void Map::checkDuplicateCnts() {
+    if (conti.size() != 1) {
+        for (int i = 0; i < (conti.size()); i++) {
+            for (int j = 0; j < conti.size(); j++) {
+                for (int l = 0; l < conti[i]->cnts.size(); l++) {
+                    for (int k = 0; k < conti[j]->cnts.size(); k++) {
+                        if ((conti[i]->cnts[l]->getName() == conti[j]->cnts[k]->getName())&& (conti[i]->getName() != conti[j]->getName())) {
+
+                            cout << "Duplicate of " << conti[i]->cnts[l]->getName()
+                                    << " found in " << conti[i]->getName()
+                                    << " and " << conti[j]->getName() << endl;
+                            return;
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    cout << "No country dupes!" << endl;
+}
+//shows all connections
+
+string Map::printEdges() {
+    string temp = "";
+    for (int i = 0; i < edges.size(); i++) {
+        temp += "[" + edges[i].first.getName() + ", " + edges[i].second.getName() + "]\n";
+    }
+
+    return temp;
+}
+
+/*=============================================================
+ CONTINENT
+ ==============================================================
+ */
+
+Continent::Continent() {
+    name = "";
+    cnts = {};
+
+}
+
+Continent::Continent(string nem) {
+    name = nem;
+    cnts = {};
+
+}
+
+void Continent::setName(string n) {
+    name = n;
+}
+
+string Continent::getName() {
+    return name;
+}
+
+string Continent::toString() {
+    return "Name: " + name + "\n";
+}
+//takes a pointer of a country
+
+void Continent::addCountry(Country* c) {
+    cnts.push_back(c);
+}
+
+int Continent::getCntsSize() {
+    return cnts.size();
+}
+
+//just returns a string of the names of countries within the continent
+
+string Continent::nameCountries() {
+    string temp;
+    if (cnts.empty()) {
+        return (name + " is an empty continent");
+    } else {
+        for (int i = 0; i < cnts.size(); i++) {
+            temp += getCountry(i).getName() + "\n";
+        }
+        return temp;
+    }
+}
+//returns country to be searched by name
+
+Country Continent::getCountry(string name) {
+
+    for (int i = 0; i < cnts.size(); i++) {
+        if (cnts[i]->getName() == (name))
+
+            return *cnts[i];
+    }
+}
+//returns country from index
+
+Country& Continent::getCountry(int index) {
+    return *cnts[index];
+}
+
+
+/*================================================
+ * COUNTRY
+ * ==============================================
+ */
+
+/**
+ * this is just a container for the information per country
+ */
+Country::Country() {
+    name = "";
+    playername = "";
+    armynum = 0;
+    visited = false;
+
+}
+
+Country::Country(string nam) {
+    name = nam;
+    armynum = 0;
+    playername = "";
+
+    visited = false;
+}
+
+Country::Country(string nam, string pname) {
+    playername = pname;
+    name = nam;
+    armynum = 0;
+
+    visited = false;
+}
+
+Country::Country(string nam, string pname, int army) {
+    playername = pname;
+    name = nam;
+    armynum = army;
+
+    visited = false;
+}
+
+string Country::toString() {
+
+    return "Country name: " + name + "\nRuler: " + playername + "\nArmy Size: "
+            + std::to_string(armynum);
+}
+
+string Country::getName() {
+    return name;
+}
+
+int Country::getArmyNum() {
+    return armynum;
+}
+
+void Country::setArmyNum(int a) {
+    armynum = a;
+}
+
+void Country::setName(string a) {
+    name = a;
+}
+
+void Country::setPlayerName(string a) {
+    playername = a;
+}
+
+void Country::setVisited(bool a) {
+    visited = a;
+}
+
+bool Country::getVisited() {
+    return visited;
+}
---
+++ mapmain.cpp
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Earl Steven Aromin 40004997
+ *
+ * Created on September 19, 2017, 7:31 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "Map.h"
+#include <iterator>
+using std::string;
+using std::cout;
+using std::endl;
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    Continent yo{"Dead"};\
+    Continent* yoo = &yo;
+    Continent ya{"Alive"};\
+    Continent* yas = &ya;
+    Map face{"ye"};
+    face.addContinent(yas);
+    face.addContinent(yoo);
+
+    Continent oi{"what"};
+    Continent* io = &oi;
+    
+   // face.addContinent(io);
+    Country heh{"Ey3"};
+    Country* ehh = &heh;
+    io->addCountry(ehh);
+    
+
+    Country oh{"Ye"};
+    Country oh1{"Ye1"};
+    Country oh2{"Ye2"};
+    Country oh3{"Ye3"};
+    Country oh4{"Ye4"};
+    Country oh5{"Ye5"};
+    Country oh6{"Ye6"};
+    Country oh7{"Ye7"};
+    Country oh8{"Ye8"};
+    Country oh9{"Ye9"};
+
+
+    Country* ohh = &oh;
+    Country* ohh1 = &oh1;
+    Country* ohh2 = &oh2;
+    Country* ohh3 = &oh3;
+    Country* ohh4 = &oh4;
+    Country* ohh5 = &oh5;
+    Country* ohh6 = &oh6;
+    Country* ohh7 = &oh7;
+    Country* ohh8 = &oh8;
+    Country* ohh9 = &oh9;
+
+
+    yoo->addCountry(ohh);
+    yoo->addCountry(ohh1);
+    yoo->addCountry(ohh2);
+    yoo->addCountry(ohh3);
+    yoo->addCountry(ohh4);
+    yoo->addCountry(ohh5);
+    yoo->addCountry(ohh6);
+    yoo->addCountry(ohh7);
+    yoo->addCountry(ohh8);
+    yoo->addCountry(ohh9);
+
+    Country ho{"Ey"};
+    Country ho1{"Ey1"};
+    Country ho2{"Ey2"};
+    Country ho3{"Ey3"};
+
+    Country* ha = &ho;
+    Country* ha1 = &ho1;
+    Country* ha2 = &ho2;
+    Country* ha3 = &ho3;
+
+    yas->addCountry(ha);
+    yas->addCountry(ha1);
+    yas->addCountry(ha2);
+    yas->addCountry(ha3);
+    
+    face.link(ho,ho1);
+    face.link(ho1,ho2);
+    face.link(ho2,ho3);
+
+  //  links countries
+    face.link(oh, oh1);
+    face.link(oh, oh2);
+    face.link(oh2, oh3);
+    face.link(oh3, oh4);
+    face.link(oh2, oh4);
+    face.link(oh4, oh5);
+    face.link(oh4, oh6);
+    face.link(oh6, oh7);
+    face.link(oh7, oh8);
+
+    face.link(oh8, oh9);
+
+    //face.link(oh9,oh1);
+
+
+    face.link(oh6,ho2);
+
+    face.checkDuplicateCnts();
+
+    //cout << face.getContinent(0)->nameCountries(); //pointer testing
+    //cout << yo.nameCountries(); //pointer testing p2
+     face.valiCont(yas);
+     face.validate();
+    return 0;
+}
+

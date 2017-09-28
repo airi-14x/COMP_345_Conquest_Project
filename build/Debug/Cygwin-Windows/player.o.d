8c6e5241c0f420267b992f9e1ca82470d0ca8536
Andrew Smith <andrew.smith5569@gmail.com>
2017-09-28 11:59:44 -0400

    added build and .dep.inc to gitignore

--- build/Debug/Cygwin-Windows/player.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/Cygwin-Windows/player.o: player.cpp player.h Dice.h
-
-player.h:
-
-Dice.h:
--- build/Debug/MinGW-Windows/cards.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/MinGW-Windows/cards.o: cards.cpp cards.h Map.h
-
-cards.h:
-
-Map.h:
